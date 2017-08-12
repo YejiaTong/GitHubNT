3653b64af88452d60f83f8ab0e91879bed491cc8
Noah Tong <Noah Tong@DESKTOP-FG8T0HA>
2017-08-12 17:55:10 -0600

    updates...2017-08-12

--- TheTongGroup/CompanyWebsite/js/send.js
+++ TheTongGroup/CompanyWebsite/js/send.js
@@ -3,4 +3,9 @@ $(document).ready(function () {
         alert("Site is under maintenance.");
         event.preventDefault();
     });
+
+    $(".team-link").on("click", function () {
+        var url = $(this).data("link-url");
+        $(location).attr("href", url);
+    });
 });
