790c43467ca3fdda6fe7107297b802ced1b8c75b
Noah Tong <Noah Tong@DESKTOP-FG8T0HA>
2017-08-16 22:15:42 -0600

    updates...2017-08-16

--- TheTongGroup/CompanyWebsite/js/send.js
+++ TheTongGroup/CompanyWebsite/js/send.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
     $("#send-form").submit(function (event) {
-        alert("Site is under maintenance.");
+        alert("Please be advised site is under maintenance.\nPlease contact at 1 (519) 562-6035 or noah089736@gmail.com");
         event.preventDefault();
     });
 
     $(".team-link").on("click", function () {
         var url = $(this).data("link-url");
-        $(location).attr("href", url);
+        if (url.match("^http")) {
+            window.open(url, "_blank")
+        } else {
+            $(location).attr("href", url);
+        }
     });
 });
